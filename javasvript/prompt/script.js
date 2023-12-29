9380ce26fc8ccd303b31d27a1ba8063cbf18e72f
Ankit <137598134+ankitkanzariya@users.noreply.github.com>
2023-12-30 00:22:21 +0530

    Add files via upload

---
+++ javasvript/prompt/script.js
@@ -0,0 +1,21 @@
+alert("hello this is alert")
+
+alert("you are ready to question")
+
+
+
+let answer = prompt('What do you call a group of lions?');
+
+if (answer == 'pride') {
+  alert('You got it right!');
+} else {
+    alert('you are wrong');
+}
+
+// let age=17;
+
+// if(age>18){
+//   console.log('yes u can')
+// }else{
+//   console.log('you cant vote')
+// }
